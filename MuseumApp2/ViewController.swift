823e0912bbd19e6b92f951dfb0cd2f4eb4a78ffd
[Waltimore] <[davidvdvelden@hotmail.com]>
2018-01-09 09:28:54 +0100

    Initial Commit

---
+++ MuseumApp2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MuseumApp2
+//
+//  Created by David van der Velden on 09/01/2018.
+//  Copyright © 2018 David van der Velden. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
